25fcc66b7fb83a3a5e38dd632457e5bfae4e6cbf
kotenak <128995380+kotenak@users.noreply.github.com>
2025-01-19 19:04:07 +0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
-...
